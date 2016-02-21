6e60129834603d6f0698e85585532f33c5f48504
Artem <namitos@users.noreply.github.com>
2016-02-21 20:08:33 +0300

    Update Model.js

--- Model.js
+++ Model.js
@@ -3,23 +3,23 @@
 class Model {
 	constructor(properties, options) {
 		properties = properties || {};
-		Object.keys(properties).forEach(function (prop) {
+		Object.keys(properties).forEach((prop) => {
 			this[prop] = properties[prop];
-		}.bind(this));
+		});
 		if (options) {
-			Object.keys(options).forEach(function (prop) {
+			Object.keys(options).forEach((prop) => {
 				Object.defineProperty(this, prop, {
 					value: options[prop]
 				});
-			}.bind(this));
+			});
 		}
 	}
 
 	toJSON() {
 		var result = {};
-		Object.keys(this).forEach(function (prop) {
+		Object.keys(this).forEach((prop) => {
 			result[prop] = this[prop];
-		}.bind(this));
+		});
 		return result;
 	}
 
@@ -46,18 +46,17 @@ class Model {
 	}
 
 	save() {
-		var model = this;
-		return new Promise(function (resolve, reject) {
-			model.constructor.sync(
-				model.constructor.schema.name,
-				model._id ? 'update' : 'create',
-				model.toJSON()
-			).then(function (result) {
-				Object.keys(result).forEach(function (key) {
-					model[key] = result[key];
+		return new Promise((resolve, reject) => {
+			this.constructor.sync(
+				this.constructor.schema.name,
+				this._id ? 'update' : 'create',
+				this.toJSON()
+			).then((result) => {
+				Object.keys(result).forEach((key) => {
+					this[key] = result[key];
 				});
-				resolve(model);
-			}, function (err) {
+				resolve(this);
+			}, (err) => {
 				reject(err);
 			});
 		});
@@ -86,11 +85,10 @@ class Model {
 	}
 
 	static read(where, options, connections) {
-		var This = this;
-		return This.sync(This.schema.name, 'read', {}, where, options, connections).then(function (loaded) {
-			return new Promise(function (resolve, reject) {
-				resolve(loaded.map(function (item) {
-					var item = new This(item);
+		return this.sync(this.schema.name, 'read', {}, where, options, connections).then((loaded) => {
+			return new Promise((resolve, reject) => {
+				resolve(loaded.map((item) => {
+					var item = new this(item);
 
 					if (item.connections) {
 						var connections = item.connections;
@@ -107,8 +105,7 @@ class Model {
 	}
 
 	static sync(collection, method, data, where, options, connections) {
-		var This = this;
-		return new Promise(function (resolve, reject) {
+		return new Promise((resolve, reject) => {
 			var toSend = {
 				collection: collection,
 				data: data,
@@ -116,7 +113,7 @@ class Model {
 				options: options,
 				connections: connections
 			};
-			This.transport.emit('data:' + method, toSend, function (data) {
+			this.transport.emit('data:' + method, toSend, (data) => {
 				if (data.hasOwnProperty('error')) {
 					reject(data.error);
 				} else {
@@ -139,25 +136,29 @@ class Model {
 class Schema {
 	constructor(schema) {
 		schema = schema || {};
-		Object.keys(schema).forEach(function (prop) {
-			this[prop] = schema[prop];
-		}.bind(this));
-	}
-
-	forEach(fn) {
-		var schema = this;
-		fn(schema);
 		if (schema.type == 'object') {
-			Object.keys(schema.properties).forEach(function (key) {
-				fn(schema.properties[key]);
+			Object.keys(schema).forEach((prop) => {
+				this[prop] = schema[prop];
 			});
 		} else if (schema.type == 'array') {
-			fn(schema.items);
+
+		}
+	}
+
+	forEach(fn, schema) {
+		schema = schema || this;
+		fn(schema);
+		if (schema.type == 'object') {
+			Object.keys(schema.properties).forEach((key) => {
+				this.forEach(fn, schema.properties[key]);
+			});
+		} else if (schema.type == 'array') {
+			this.forEach(fn, schema.items);
 		}
 	}
 
 	getField(path, property) {
-		var schemaPath = _.flatMap(path.split('.'), function (part) {
+		var schemaPath = _.flatMap(path.split('.'), (part) => {
 			return ['properties', part];
 		});
 		var field = _.get(this, schemaPath.join('.'));
